5ee5d28a5d0efaf3a290d8dcfbbb55b15ca7f2d2
Mang-developer <chiwon618@gmail.com>
2020-04-20 16:04:59 +0900

    Docs: js init setting

--- public/js/goal.js
+++ public/js/goal.js
@@ -1,3 +1,31 @@
 // goal.js
+// 상태
+let goals = [];
+
+// Dom
+const $goalList = document.querySelector('.goalList');
+
+const now = new Date();
+console.log(now);
+
+// 함수
+const goalRender = () => {
+  let html = '';
+  goals.forEach(goal => {
+    const goalDday = goal.dDay.split('-');
+    html += `<li class="${goal.color}">
+    <div class="goalTit">
+      <h4>${goal.content}</h3>
+      <span class="closingDate">${goalDday[0]}.${goalDday[1]}.${goalDday[2]} / D-${0}</span>
+    </div>
+    <button class="btnEdit">수정</button>
+    <button class="btnDelete">삭제</button>
+  </li>`;
+  });
+  $goalList.innerHTML = html;
+};
+
 fetch('../../goals')
-  .then(console);
+  .then(res => res.json())
+  .then(res => goals = res)
+  .then(goalRender);

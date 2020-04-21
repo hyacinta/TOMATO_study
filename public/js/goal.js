e1a15174b388a7753b1b39cd9c4d5dc85b194bdb
Mang-developer <chiwon618@gmail.com>
2020-04-21 13:55:08 +0900

    full checkout

--- public/js/goal.js
+++ public/js/goal.js
@@ -8,12 +8,34 @@ const oneHour = 3600000;
 const oneDay = 86400000;
 
 // Dom
+const $btnAddGoal = document.querySelector('.btnAddGoal');
 const $addGoal = document.querySelector('.addGoal');
+const $deleteGoal = document.querySelector('.deleteGoal');
 const $goalList = document.querySelector('.goalList');
 
-// 함수
-const dDay = date => Math.ceil((date - now) / oneDay);
+let targetId = null;
 
+// 함수
+// 현재 날짜부터 남은 날짜 구하는 함수
+const dDay = date => Math.ceil((date - now) / oneDay);
+// popup 여는 함수
+const openPopup = target => {
+  target.classList.add('active');
+};
+// popup 닫는 함수
+const closePopup = target => {
+  // target.
+  target.classList.remove('active');
+};
+// popup 이벤트 함수
+const popup = (target, $popup, callback) => {
+  if (target.matches('.btnRegister') || target.matches('.btnDelete')) {
+    callback();
+  } else {
+    closePopup($popup);
+  }
+};
+// 목표 페이지 랜더 함수
 const goalRender = () => {
   let html = '';
   goals.forEach(goal => {
@@ -31,14 +53,42 @@ const goalRender = () => {
 };
 
 // 이벤트 핸들러
+// 목표 추가 버튼 이벤트
+$btnAddGoal.onclick = () => {
+  if (goals.length >= 5) {
+    window.alert('목표는 최대 5개 입니다.');
+    return;
+  }
+  openPopup($addGoal);
+};
+// 목표 추가 popup 이벤트
+$addGoal.onclick = ({ target }) => {
+  if (!target.matches('button')) return;
+  popup(target, $addGoal, () => {
+    console.log('목표 추가 이벤트');
+  });
+};
+// 목표 요소 안 수정/삭제 버튼
 $goalList.onclick = ({ target }) => {
   if (!(target.matches('.btnEdit') || target.matches('.btnDelete'))) return;
-  if (target.matches('.btnEdit')) $addGoal.classList.add('active');
+  targetId = target.parentNode.id;
+  if (target.matches('.btnEdit')) {
+    openPopup($addGoal);
+  } else {
+    openPopup($deleteGoal);
+  }
 };
-
-window.onload = () => {
-  fetch('../../goals')
-    .then(res => res.json())
-    .then(res => goals = res)
-    .then(goalRender);
+// 삭제 확인 popup 이벤트
+$deleteGoal.onclick = ({ target }) => {
+  if (!target.matches('button')) return;
+  popup(target, $deleteGoal, () => {
+    console.log('삭제 이벤트');
+  });
+  targetId = null; 
+};
+// 로드 이벤트
+window.onload = async () => {
+  const _goals = await fetch('../../goals').then(res => res.json());
+  goals = _goals;
+  goalRender();
 };

69d9ea244f432e8ddf0f11524c6351dfea61dd64
Leehaeun0 <a01050675111@gmail.com>
2020-04-22 06:19:44 +0900

    feat: add editTodo popup page fetch for main.js

--- public/js/main.js
+++ public/js/main.js
@@ -24,8 +24,6 @@ const progressBar = (done, goal) => {
   const [doneHour, doneMin] = [+(doneArr[0] + doneArr[1]), +(doneArr[2] + doneArr[3])];
   const [goalHour, goalMin] = [+goalArr[0], +(goalArr[1] + goalArr[2])];
 
-  console.log(doneHour, doneMin, goalHour, goalMin);
-
   let percent = Math.round(((doneHour * 60 + doneMin) / (goalHour * 60 + goalMin)) * 100);
   if (isNaN(percent)) percent = 0; 
   return percent = percent > 100 ? 101 : percent;
@@ -114,6 +112,13 @@ const getTodayPersent = () => {
   $todayPercent.textContent = `${percent}%`;
 };
 
+const getToday = _todos => {
+  todos = _todos.filter(todo => {
+    const today = new Date();
+    const month = today.getMonth() + 1 > 9 ? today.getMonth() + 1 : `0${today.getMonth() + 1}`;
+    return todo.date === `${today.getFullYear()}-${month}-${today.getDate()}`; // --
+  });
+};
 
 const getTodos = async () => {
   try {
@@ -122,24 +127,24 @@ const getTodos = async () => {
     renderGoals();
 
     todos = await fetch('/todos').then(res => res.json());
-
+    getToday(todos);
+    getTodayDone();
+    render();
+    getTodayGoalTIme();
+    getTodayPersent();
   } catch (e) {
     console.error('Error:', e);
   }
 
-  await fetch('/todos')
-    .then(res => res.json())
-    .then(data => data.filter(todo => {
-      const today = new Date();
-      const month = today.getMonth() + 1 > 9 ? today.getMonth() + 1 : `0${today.getMonth() + 1}`;
-      return todo.date === `${today.getFullYear()}-${month}-${today.getDate() + 1}`; // --
-    }))
-    .then(_todos => todos = _todos)
-    .then(getTodayDone)
-    .then(render)
-    .then(getTodayGoalTIme)
-    .then(getTodayPersent)
-    .catch(error => console.error('Error:', error));
+  // await fetch('/todos')
+  //   .then(res => res.json())
+  //   .then(data => getToday(data))
+  //   .then(_todos => todos = _todos)
+  //   .then(getTodayDone)
+  //   .then(render)
+  //   .then(getTodayGoalTIme)
+  //   .then(getTodayPersent)
+  //   .catch(error => console.error('Error:', error));
 };
 
 window.onload = getTodos;
@@ -147,7 +152,6 @@ window.onload = getTodos;
 const removeActive = () => {
   $timerPopup.classList.remove('active');
 };
-
 
 const timerClosure = (() => {
   // let count = 0;
@@ -216,8 +220,6 @@ const startStopWatch = () => {
   if (popupControl.containPlay()) return;
   
   const timer = setInterval(() => {
-    // console.log(popupControl.containPlay());
-
     if (popupControl.containPlay()) {
       clearInterval(timer);
       return;
@@ -251,7 +253,6 @@ const renderPopup = target => {
 };
 
 const addDetail = target => {
-  // todos.forEach(todo => todo.id === +target.id ? todo.com)
   target.parentNode.classList.toggle('ing');
   console.log(target);
 };
@@ -284,82 +285,217 @@ const deleteTodo = target => {
   console.log(target.parentNode);
 };
 
-const editTodo = target => {
+const selectGoals = todo => {
+  const $selectGoals = document.querySelector('.addTodos .addInput .category select');
+  let html = `<option value="${todo.goal}">${goals.find(goal => goal.id === +todo.goal).content}</option>`;
+  goals.forEach(goal => {
+    if (goal.id === todo.goal) return;
+    html += `<option value="${goal.id}">${goal.content} <span class="dDay" style="font-size: 1.6rem;">D-30</span></option>`;
+  });
+  $selectGoals.innerHTML = html;
+};
+
+const getGoalTime = ({ goalTime }) => {
+  if (goalTime === '0:30') return 1;
+  if (goalTime === '1:00') return 2;
+  if (goalTime === '1:30') return 3;
+  if (goalTime === '2:00') return 4;
+  if (goalTime === '2:30') return 5;
+  if (goalTime === '3:00') return 6;
+  if (goalTime === '3:30') return 7;
+  if (goalTime === '4:00') return 8;
+  if (goalTime === '4:30') return 9;
+  if (goalTime === '5:00') return 10;
+};
+
+const giveValue = todo => {
+  const $startMin = document.querySelector('.addTodos .startTime select');
+  const $startHour = document.querySelector('.addTodos .startTime input');
+  const $goalTime = document.querySelector('.addTodos .goalTime select');
+  const $startDate = document.querySelector('.addTodos li.startDate input');
+  const $todoInput = document.querySelector('.addInput > .todoInput input');
+
+  // const $endDate = document.querySelector('.addTodos li.endDate input');
+
+  $startMin.value = +todo.startTime[3] + 1;
+  $startHour.value = todo.startTime[0] + todo.startTime[1];
+  $goalTime.value = getGoalTime(todo);
+  $startDate.value = todo.date;
+  $todoInput.value = todo.content;
+  // $endDate.value = 
+
+  return {
+    $startMin, $startHour, $goalTime, $startDate 
+  };
+};
+
+// <button class="btnImp ${todo.important ? ' impCheck' : ''}" id="test">중요</button>
+const renderEditTodo = target => {
   $addTodosPopup.classList.add('active');
 
-  
-  $addTodosPopup.innerHTML = `
-  <h3>할일 추가</h3>
-    <button class="btnClosed">닫기</button>
-    <ul class="addInput">
-      <li class="category">
-        <label for="" class="a11yHidden">목표 선택</label>
-        <select id="categorySelect" class="categorySelect">
-          <option value="">목표를 선택하세요</option>
-        </select>
-      </li>
-      <li class="todoInput">
-        <label for="" class="a11yHidden">할일 입력</label>
-        <input type="text" placeholder="할일을 입력하세요">
-      </li>
-      <li class="impSelect"><label for="test" class="a11yHidden">text</label>
-        <button class="btnImp" id="test">중요</button>
-      </li>
-      <li class="startDate">
-        <label for="">시작 날짜</label>
-        <input type="date" name="" id="">
-      </li>
-      <li class="endDate">
-        <label for="">종료 날짜</label>
-        <input type="date" name="" id="">
-      </li>
-      <li class="startTime">
-        <label for="">공부 시작시간</label>
-        <input type="number" name="" id="" placeholder="시">
-        <select name="country" id="countrySelectBox">
-          <option value="">00분</option>
-          <option value="">10분</option>
-          <option value="">20분</option>
-          <option value="">30분</option>
-          <option value="">40분</option>
-          <option value="">50분</option>
-        </select>
-      </li>
-      <li class="goalTime">
-        <label for="">목표 공부시간</label>
-        <select name="country" id="countrySelectBox">
-          <option value="">30분</option>
-          <option value="">1시간</option>
-          <option value="">1시간 30분</option>
-          <option value="">2시간</option>
-          <option value="">2시간 30분</option>
-          <option value="">3시간</option>
-          <option value="">3시간 30분</option>
-          <option value="">4시간</option>
-          <option value="">4시간 30분</option>
-          <option value="">5시간</option>
-        </select>
-      </li>
-      <li class="contentInput">
-        <label for="" class="a11yHidden">상세 내용 입력</label>
-        <textarea name="" id="" cols="30" rows="10" placeholder="상세 내용을 입력하세요"></textarea>
-      </li>
-    </ul>
-    <button class="btnCancel">취소</button>
-    <button class="btnRegister">등록</button>
-  </div>
-  <div class="dimd"></div>`;
-
+  todos.forEach(todo => {
+    if (todo.id !== +target.parentNode.id) return;
+    $addTodosPopup.innerHTML = `
+      <h3 id="${todo.id}">할일 추가</h3>
+      <button class="btnClosed">닫기</button>
+      <ul class="addInput">
+        <li class="category">
+          <label for="" class="a11yHidden">목표 선택</label>
+          <select id="categorySelect" class="categorySelect">
+            <option value="1">목표를 선택하세요</option>
+          </select>
+        </li>
+        <li class="todoInput">
+          <label for="" class="a11yHidden">할일 입력</label>
+          <input type="text" placeholder="${todo.content}">
+        </li>
+        <li class="impSelect"><label for="test" class="a11yHidden">text</label>
+          <button class="btnImp ${todo.color}${todo.important ? ' impCheck' : ''}" id="test">중요</button>
+        </li>
+        <li class="startDate">
+          <label for="">시작 날짜</label>
+          <input type="date" name="" id="">
+        </li>
+        <li class="endDate">
+          <label for="">종료 날짜</label>
+          <input type="date" name="" id="">
+        </li>
+        <li class="startTime">
+          <label for="">공부 시작시간</label>
+          <input type="number" name="" id="" placeholder="시">
+          <select name="country" id="countrySelectBox">
+            <option value="1">00분</option>
+            <option value="2">10분</option>
+            <option value="3">20분</option>
+            <option value="4">30분</option>
+            <option value="5">40분</option>
+            <option value="6">50분</option>
+          </select>
+        </li>
+        <li class="goalTime">
+          <label for="">목표 공부시간</label>
+          <select name="country" id="countrySelectBox">
+            <option value="1">30분</option>
+            <option value="2">1시간</option>
+            <option value="3">1시간 30분</option>
+            <option value="4">2시간</option>
+            <option value="5">2시간 30분</option>
+            <option value="6">3시간</option>
+            <option value="7">3시간 30분</option>
+            <option value="8">4시간</option>
+            <option value="9">4시간 30분</option>
+            <option value="10">5시간</option>
+          </select>
+        </li>
+        <li class="contentInput">
+          <label for="" class="a11yHidden">상세 내용 입력</label>
+          <textarea name="" id="" cols="30" rows="10" placeholder="상세 내용을 입력하세요">${todo.detail}</textarea>
+        </li>
+      </ul>
+      <button class="btnCancel">취소</button>
+      <button class="btnRegister">등록</button>`;
+    
+    giveValue(todo);
+    selectGoals(todo);
+  });
 };
 
 const removeEdit = () => {
   $addTodosPopup.classList.remove('active');
 };
 
+const addZero = num => {
+  return num.length > 1 || num > 9 ? num : '0' + num;
+};
+
+const getContent = () => {
+  const $todoInput = document.querySelector('.addInput > .todoInput input');
+  return $todoInput.value;
+};
+
+const getGoal = () => {
+  const $selectGoals = document.querySelector('.addTodos .addInput .category select');
+  return +$selectGoals.value;
+};
+
+const getColor = () => {
+  return goals.find(goal => goal.id === getGoal()).color;
+};
+
+const getDate = () => {
+  const $startDate = document.querySelector('.addTodos li.startDate input');
+  return $startDate.value;
+};
+
+const getDayNum = () => {
+  const date = getDate();
+  const day = new Date(+(date[0] + date[1] + date[2] + date[3]), +(date[5] + date[6]) - 1, +(date[8] + date[9]));
+  return day.getDay();
+};
+
+const getStart = () => {
+  const $startHour = document.querySelector('.addTodos .startTime input');
+  const $startMin = document.querySelector('.addTodos .startTime select');
+  return `${addZero($startHour.value)}:${addZero(($startMin.value - 1) * 10)}`;
+};
+
+const getImp = () => {
+  const $btnImp = document.querySelector('.addTodos li.impSelect .btnImp');
+  return $btnImp.classList.contains('impCheck');
+};
+
+const getDetail = () => {
+  const $detail = document.querySelector('.addTodos li.contentInput > textarea');
+  return $detail.value;
+};
+
+const getGoalTm = () => {
+  const $goalTime = +document.querySelector('.addTodos .goalTime select').value;
+  console.log($goalTime);
+  if ($goalTime === 1) return '0:30';
+  if ($goalTime === 2) return '1:00';
+  if ($goalTime === 3) return '1:30';
+  if ($goalTime === 4) return '2:00';
+  if ($goalTime === 5) return '2:30';
+  if ($goalTime === 6) return '3:00';
+  if ($goalTime === 7) return '3:30';
+  if ($goalTime === 8) return '4:00';
+  if ($goalTime === 9) return '4:30';
+  if ($goalTime === 10) return '5:00';
+}
+
+const editTodo = target => {
+  // 인풋이 비어있으면 리턴하기 추가
+  getGoalTm();
+  const id = +target.parentNode.firstElementChild.id;
+  fetch(`/todos/${id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      content: getContent(),
+      goal: getGoal(),
+      color: getColor(),
+      date: getDate(),
+      day: getDayNum(),
+      important: getImp(),
+      startTime: getStart(),
+      goalTime: getGoalTm(),
+      detail: getDetail(),
+    })
+  }).then(res => res.json())
+    .then(data => todos = todos.map(todo => (todo.id === id ? data : todo)))
+    .then(getToday) 
+    .then(render)
+    .then(removeEdit) 
+    .catch(error => console.error('Error:', error));
+};
+
+// 투두 수정 팝업창
 $addTodosPopup.onclick = e => {
   if (e.target.matches('.addTodos > .btnCancel')) removeEdit();
   if (e.target.matches('.addTodos > .btnClosed')) removeEdit();
-  // if (e.target.matches('.addTodos > .btnRegister')) removeEdit();
+  if (e.target.matches('.addTodos > .btnRegister')) editTodo(e.target);
+  if (e.target.matches('.addInput > li.impSelect .btnImp')) e.target.classList.toggle('impCheck');
 };
 
 // 투두리스트 클릭 > 1.스탑워치 시작 버튼
@@ -368,8 +504,9 @@ $todoList.onclick = e => {
   if (e.target.matches('.todoList > li > a > h4 > .icoImp')) toggleCheck(e.target);
   if (e.target.matches('.todoList > li > .todoTitSet')) addDetail(e.target);
   if (e.target.matches('.todoList > li > .todoTitSet > *')) addDetail(e.target.parentNode);
+  if (e.target.matches('.todoList > li')) e.target.classList.toggle('ing');
   if (e.target.matches('.todoList > li > .btnDelete')) deleteTodo(e.target);
-  if (e.target.matches('.todoList > li > .btnEdit')) editTodo(e.target);
+  if (e.target.matches('.todoList > li > .btnEdit')) renderEditTodo(e.target);
 };
 
 const patchTimer = target => {
@@ -396,3 +533,4 @@ $timerPopup.onclick = e => {
     if (!popupControl.containPlay()) startStopWatch();
   }
 };
+
